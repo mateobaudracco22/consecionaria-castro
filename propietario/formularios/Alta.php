a931dd3b8a5cda777e64de6a03e397d11614705d
Baudraccomateo <mateobaudracco@gmail.com>
2025-11-20 11:40:52 -0300

    Nuevos cambios

--- propietario/formularios/Alta.php
+++ propietario/formularios/Alta.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Alta Propietario</title>
-  <link rel="stylesheet" href="../css/style_alta.css">
+  <link rel="stylesheet" href="../../css/style_alta.css">
 </head>
 <body>
   <h1>Concesionaria</h1>
--- propietario/formularios/SolicitarDatosActualizar.php
+++ propietario/formularios/SolicitarDatosActualizar.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>Actualizar Propietario</title>
+  <link rel="stylesheet" href="../../css/style_actualizar.css">
 </head>
 <body>
 
